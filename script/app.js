6bc449504a445fa53c7bb5797f71ee5644126aa0
moinsheikh007 <mohinshei014@gmail.com>
2024-01-07 22:28:01 +0600

    To-Do-List Design

--- script/app.js
+++ script/app.js
@@ -1,1 +1,35 @@
-console.log('Hello Minor Moin')
+const inputFiled = document.getElementById('inputField');
+
+const listContainer = document.getElementById('listContainer');
+
+inputFiled.addEventListener('keypress', function (event) {
+    if(event.key === 'Enter') {
+        event.preventDefault();
+        document.getElementById('addBtn').click();
+    }
+})
+
+function addTask () {
+    if(inputFiled.value === '') {
+        alert('You Must Write Something')
+    }
+    else {
+        const li = document.createElement('li');
+        li.innerHTML = inputFiled.value;
+        listContainer.appendChild(li);
+
+        const span = document.createElement('span');
+        span.innerHTML = '&#10005';
+        li.appendChild(span)
+    }
+    inputFiled.value = '';
+}
+
+listContainer.addEventListener('click', function (event) {
+    if (event.target.tagName === 'LI') {
+        event.target.classList.toggle('active');
+    }
+    else if (event.target.tagName === 'SPAN') {
+        event.target.parentElement.remove(event.target)
+    }
+})
